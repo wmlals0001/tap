9086ceee5d54ac7ad33ca79438df21f7de81de8d
Tap <wmlals0001@naver.com>
2018-10-12 00:11:39 +0900

    10-11 메인사진수정

--- js/common.js
+++ js/common.js
@@ -54,7 +54,7 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.1em solid #444 }";
   document.body.appendChild(css);
 };
 
