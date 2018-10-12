ce1d92844c1ef5d1b3e2d5386f314c787dedaa5c
tap <wmlals0001@naver.com>
2018-10-12 12:09:20 +0900

    10-12 메인수정

--- js/common.js
+++ js/common.js
@@ -1,63 +1,6 @@
 	
 
 
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.1em solid #444 }";
-  document.body.appendChild(css);
-};
-
 
 $(document).ready(function(){
 
@@ -80,21 +23,27 @@ $( window ).resize( function() {
 });
 
 
-$('header>ul>li:nth-child(1)').click(function(){
-	$("html, body").stop().animate({scrollTop:-65},1000);
-});
+    $('header>ul>li:nth-child(1)').click(function(){
+        $("html, body").stop().animate({scrollTop:-65},1000);
+    });
 
-$('header>ul>li:nth-child(2)').click(function(){
-	$("html, body").stop().animate({scrollTop:Top1-65},1000);
-});
+    $('header>ul>li:nth-child(2)').click(function(){
+        $("html, body").stop().animate({scrollTop:Top1-65},1000);
+    });
 
-$('header>ul>li:nth-child(3)').click(function(){
-	$("html, body").stop().animate({scrollTop:Top3-65},1000);
-});
+    $('header>ul>li:nth-child(3)').click(function(){
+        $("html, body").stop().animate({scrollTop:Top3-65},1000);
+    });
 
-$('header>ul>li:nth-child(4)').click(function(){
-	$("html, body").stop().animate({scrollTop:Top7-65},1000);
-});
+    $('.main_text button').click(function(){
+        $("html, body").stop().animate({scrollTop:Top3-65},1000);
+    });
+
+
+
+    $('header>ul>li:nth-child(4)').click(function(){
+        $("html, body").stop().animate({scrollTop:Top7-65},1000);
+    });
 
 
 
